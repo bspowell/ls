8e26738cac781d15ae0f01d535c2531f6c077c46
bspowell <b.sean.powell@gmail.com>
2023-02-01 19:50:41 +0000

    adding rock paper scissors files

---
+++ rb_101/lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,46 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+    prompt("You chose: #{player}; computer chose: #{computer}")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie")
+  end
+end
+
+choice = ''
+loop do
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing. bye!")
---
+++ rb_101/lesson_2/rpsls_bonus.rb
@@ -0,0 +1,74 @@
+CHOICES = %w(rock paper scissors lizard spock)
+WIN_CONDITION = [%w(rock scissors), %w(rock lizard), %w(paper rock),
+                 %w(paper spock), %w(scissors paper), %w(scissors lizard),
+                 %w(lizard spock), %w(lizard paper), %w(spock rock),
+                 %w(spock scissors)]
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def validate_choice(user_choice)
+  CHOICES.any?(Regexp.new(user_choice))
+end
+
+def choice_to_word(player_choice)
+  word = CHOICES.select do |element|
+    element.start_with?(Regexp.new(player_choice))
+  end
+  word.join()
+end
+
+def who_wins?(player, computer)
+  WIN_CONDITION.any? do |element|
+    element[0] == player && element[1] == computer
+  end
+end
+
+player_score = 0
+computer_score = 0
+tie_score = 0
+
+prompt("Welcome to RPSLS!")
+loop do
+  user_choice = ''
+  loop do
+    prompt("Choose one of the following: rock, paper, scissors, lizard, spock")
+    user_choice = gets.chomp
+
+    if user_choice == 's'
+      prompt("Do you mean spock or scissors?")
+      next
+    end
+
+    break if validate_choice(user_choice)
+
+    prompt("Enter a valid option")
+  end
+
+  user_choice = choice_to_word(user_choice)
+  computer_choice = CHOICES.sample
+
+  if who_wins?(user_choice, computer_choice)
+    prompt("You won!")
+    player_score += 1
+  elsif who_wins?(computer_choice, user_choice)
+    prompt("Computer won!")
+    computer_score += 1
+  else
+    prompt("It's a tie!")
+    tie_score += 1
+  end
+
+  prompt("You chose #{user_choice}, computer chose #{computer_choice}")
+  prompt("Score (Win,Loss,Tie): #{player_score},#{computer_score},#{tie_score}")
+  prompt("Would you like to play again? (Y to play again)")
+  play_again = gets.chomp
+  if play_again.downcase.start_with?('y')
+    next
+  else
+    break
+  end
+end
+
+prompt("Goodbye!")
