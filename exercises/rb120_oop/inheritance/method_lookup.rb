cf24219fc9e36f7073f45f49532e770d0b4fe031
bspowell <b.sean.powell@gmail.com>
2023-05-02 18:27:54 +0000

    May 1st - rb120

---
+++ exercises/rb120_oop/inheritance/method_lookup.rb
@@ -0,0 +1,27 @@
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new('Black')
+cat1.color
+
+
+=begin
+Look up path
+
+cat
+animal
+since it stops at animal it stops.
+
+=end
+
