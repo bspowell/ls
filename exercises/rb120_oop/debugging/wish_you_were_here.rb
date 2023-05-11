4c5e07bc3570f19cdc8d9380120a110f73096f9e
bspowell <82974206+bspowell@users.noreply.github.com>
2023-05-11 15:22:19 -0700

    May 11th: debugging exercises

---
+++ exercises/rb120_oop/debugging/wish_you_were_here.rb
@@ -0,0 +1,53 @@
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+
+end
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+
+  def ==(other)
+    latitude == other.latitude && longitude == other.longitude
+  end
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location # expected: true
+                                    # actual: false
+
+=begin
+ For custom classes, the == operator returns true if and only if the two objects are the same instance.
+Since we are actually asking if the two objects ada and grace, contain the same coordinate values (latitude and logitude)
+within their respective GeoLocation objects. 
+
+We want to 
+=end
